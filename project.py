46e9ecca8be7e38315d92cdac2ba7b137bc67513
cvgutierrez <89503742+cvgutierrez@users.noreply.github.com>
2021-11-29 00:36:56 -0700

    Redid all the functions "no romanization yet"

--- project.py
+++ project.py
@@ -32,20 +32,13 @@ def get_args():
                         metavar='str',
                         type=str,
                         default='English')
-    
-    parser.add_argument('file',
-                        help='Input file to translate',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None,
-                        nargs='*')
-    
-    parser.add_argument('string',
-                        help="Input string to translate",
+
+    parser.add_argument('-i',
+                        '--input',
+                        help="Input to translate",
                         metavar='str',
                         type=str,
-                        default=None,
-                        nargs='*')
+                        default=None)
       
     parser.add_argument('-o',
                         '--outdir',
@@ -62,41 +55,55 @@ def main():
     '''Main Function that calls upon other functions to translate'''
 
     args = get_args()
-    str_arg = args.arg
-    
     if not os.path.isdir(args.outdir):
         os.makedirs(args.outdir)
+    input_lang = str(args.language1).lower()
+    output_lang = str(args.language2).lower()
+    original = open(os.path.join(args.outdir, input_lang + ".txt"), 'wt')
+    output = open(os.path.join(args.outdir, output_lang + ".txt"), 'wt')
+    together = open(os.path.join(args.outdir,"Side_by_Side.txt"), 'wt')
 
-    if args.string:
-        translated = string_translate(args.string, agrs.language1, args.language2)
-        print(translated, file = args.outdir)
+    if args.input == None:
+        print("There was nothing to translate. Please try again and input either a file or string.")
+    elif os.path.isfile(args.input):
+        path = os.path.abspath(args.input)
+        translated = file_translate(path, args.language1, args.language2)
+        lines = []
+        for line in open(args.input):
+            print(line.rstrip(), file= original)
+            lines.append(line.rstrip())
+        print(translated, file= output)
+        combined = [input_lang + ': ' + '\n'.join(lines), '\n', output_lang + ": " + translated]
+        print("\n".join(combined), file=together)
+    else:
+        translated = string_translate(args.input, args.language1, args.language2)
+        print(args.input, file=original)
+        print(translated, file = output)
+        combined = [input_lang + ': ' + args.input, '\n', output_lang + ": " + translated]
+        print("\n".join(combined), file=together)
 
-    if args.file:
-        translated = file_translate(args.file, agrs.language1, args.language2)
-        print(translated, file = args.outdir)
-
-    if not args.file and not args.string:
-        print("There's nothing to translate. Please input a string or file to translate.")
-
-    
 
 # --------------------------------------------------
 def string_translate(string, in_lang, out_lang):
     '''translate strings'''
 
-    interpreter = Translator(from_lang=in_lang, to_lang=out_lang)
-    return translator.translate(string)
+    interpreter = Translator(from_lang=in_lang.lower(), to_lang=out_lang.lower())
+    return interpreter.translate(string)
 
   
 # --------------------------------------------------
-def file_translate(file, in_lang, out_lang):
-    '''translate files'''
+def file_translate(input_file, in_lang, out_lang):
+    '''translate file'''
 
     interpreter = Translator(from_lang=in_lang, to_lang=out_lang)
     lines = []
+    file = open(input_file, 'rt')
     for line in file:
-        new_line = translator.translate(line.rstrip())
+        # print(line)
+        new_line = interpreter.translate(line.rstrip())
+        # print(new_line)
         lines.append(new_line)
+
     return "\n".join(lines)
 
   
